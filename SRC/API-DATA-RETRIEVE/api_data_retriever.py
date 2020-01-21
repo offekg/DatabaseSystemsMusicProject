b894f658584186dd47ab5f39b0aa0e4936fff2fd
yaellerech <48100558+yaellerech@users.noreply.github.com>
2020-01-21 14:36:00 +0200

    added functions from api website

--- SRC/API-DATA-RETRIEVE/api_data_retriever.py
+++ SRC/API-DATA-RETRIEVE/api_data_retriever.py
@@ -5,6 +5,26 @@ import theaudiodb_retriever
 from timeit import default_timer as timer
 
 
+def get_artist_json(artist_name):
+    start = timer()
+    response_artists = requests.get("http://theaudiodb.com/api/v1/json/1/search.php?s=" + artist_name)
+    response_artists_json = response_artists.json()
+    end = timer()
+    print("Artist response time: ", end - start)
+    return response_artists_json
+
+def get_albums_for_artist_json(theaudiodb_album_id):
+    start = timer()
+    response_albums = requests.get("https://theaudiodb.com/api/v1/json/1/album.php?i=" + str(theaudiodb_album_id))
+    response_albums_json = response_albums.json()
+    end = timer()
+    print("Albums response time: ", end - start)
+    return response_albums_json
+
+def get_tracks_for_album_json(theaudiodb_album_id):
+    response_tracks = requests.get("https://theaudiodb.com/api/v1/json/1/track.php?m=" + str(theaudiodb_album_id))
+    return response_tracks.json()
+
 def retrieve_and_insert_to_database(start_index, stop_index):
     with open('artists.csv', 'r', encoding="utf-8-sig") as csvfile:
         start = timer()
