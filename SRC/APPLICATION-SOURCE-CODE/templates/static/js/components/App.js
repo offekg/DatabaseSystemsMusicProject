851cfbd53a0bb0f5c4309d807514d09b53d7767e
EizTimor <timor.eizenman@gmail.com>
2020-01-21 17:59:33 +0200

    homepae and create your own switch

--- SRC/APPLICATION-SOURCE-CODE/templates/static/js/components/App.js
+++ SRC/APPLICATION-SOURCE-CODE/templates/static/js/components/App.js
@@ -6,6 +6,24 @@ import QuerySelector from "./QuerySelector";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 
+import Fab from "@material-ui/core/Fab";
+import { makeStyles } from "@material-ui/core/styles";
+import QueueMusicIcon from "@material-ui/icons/QueueMusic";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      minHeight: "60%",
+      minWidth: "80%",
+      marginTop: theme.spacing(10)
+    }
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1)
+  }
+}));
+
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
@@ -20,14 +38,39 @@ function Copyright() {
 }
 
 export default function App() {
+  const classes = useStyles();
+
+  const [section, setSection] = React.useState(<HomePage />);
+  const [buttonText, setText] = React.useState("Create Your Own Top 10");
+
+  const handleButtonClick = () => {
+    if (buttonText === "Create Your Own Top 10") {
+      setSection(<QuerySelector />);
+      setText("Back To Our TOP 10");
+    } else {
+      setSection(<HomePage />);
+      setText("Create Your Own Top 10");
+    }
+  };
+
   return (
     <Container maxWidth="lg" align="center">
       <Box my={4}>
         <Typography variant="h2" component="h1" gutterBottom>
           <b>HaMeaser</b>
         </Typography>
-        <HomePage />
-        <QuerySelector />
+        {section}
+        <div className={classes.root}>
+          <Fab
+            size="large"
+            color="primary"
+            variant="extended"
+            onClick={handleButtonClick}
+          >
+            <QueueMusicIcon className={classes.extendedIcon} />
+            {buttonText}
+          </Fab>
+        </div>
         <Copyright />
       </Box>
     </Container>
