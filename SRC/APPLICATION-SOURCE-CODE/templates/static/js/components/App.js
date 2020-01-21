77f9b80fb4b4701fae2cfce9cd12bb5d4e30f0c8
EizTimor <timor.eizenman@gmail.com>
2020-01-21 17:37:51 +0200

    added homepage

--- SRC/APPLICATION-SOURCE-CODE/templates/static/js/components/App.js
+++ SRC/APPLICATION-SOURCE-CODE/templates/static/js/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import HomePage from "./HomePage";
 import Box from "@material-ui/core/Box";
 import Link from "@material-ui/core/Link";
 import QuerySelector from "./QuerySelector";
@@ -25,6 +26,7 @@ export default function App() {
         <Typography variant="h2" component="h1" gutterBottom>
           <b>HaMeaser</b>
         </Typography>
+        <HomePage />
         <QuerySelector />
         <Copyright />
       </Box>
